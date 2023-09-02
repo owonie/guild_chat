3135cd2618f33513f8c07d88de374f7cf4699b4a
Owon <chlehdnjs@gmail.com>
2023-09-03 03:23:57 +0900

    feat: login page layout
    feat: login page route
    feat: page layout styled component
    feat: login page component
    modify: remove basename in BroserRouter

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import PageLayout from '../../components/Layout/Layout';
+
+const LoginPage = () => {
+  return (
+    <>
+      <PageLayout>
+        <div>slider section</div>
+        <div>login form section</div>
+      </PageLayout>
+    </>
+  );
+};
+
+export default LoginPage;
