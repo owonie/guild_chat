be9ffdbfb441cbbbc634efa92b602b3b40e7d468
Owon <chlehdnjs@gmail.com>
2023-09-03 04:14:07 +0900

    feat: add mock APIs for login page
    add: signingoogle mock request API
    add: emailverify mock request API

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,13 +3,20 @@ import { rest } from 'msw';
 export const handlers = [
   rest.post('/signin', (req, res, ctx) => {
     sessionStorage.setItem('is-authenticated', 'true');
-
     return res(ctx.status(200));
+  }),
+
+  rest.post('/signingoogle', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        message: 'Gmail Login!',
+      })
+    );
   }),
 
   rest.get('/user', (req, res, ctx) => {
     const isAuthenticated = sessionStorage.getItem('is-authenticated');
-
     if (!isAuthenticated) {
       return res(
         ctx.status(403),
@@ -23,6 +30,15 @@ export const handlers = [
       ctx.status(200),
       ctx.json({
         username: 'owon',
+      })
+    );
+  }),
+
+  rest.post('emailverify', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        verifyNumber: '252132',
       })
     );
   }),
