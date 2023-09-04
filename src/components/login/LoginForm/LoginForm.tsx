eb7936b68ff775e03fbcf1960c55b74d3994306f
Owon <chlehdnjs@gmail.com>
2023-09-04 15:41:00 +0900

    feat: login form tab menu
    feat: tab menu routing
    feat: login api test

--- src/components/login/LoginForm/LoginForm.tsx
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -1,18 +1,36 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { handlers } from '../../../mocks/handlers';
 
 const emailRegEx =
   /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
 const phoneRegEx = /^[0-9\b -]{3,13}$/;
 
-// type TabState = 'email' | 'phone';
+type TabState = 'tabEmail' | 'tabPhone';
+type IdValueObject = {
+  [key: string]: string;
+  inputEmail: string;
+  inputPhone: string;
+};
 
 const LoginForm = () => {
+  const apis = handlers;
+
   const [emailError, setEmailError] = useState<boolean>(true);
   const [phoneError, setPhoneError] = useState<boolean>(true);
+  const [tabState, setTabState] = useState<TabState>('tabEmail');
+  const [passwordIsVisiable, setPasswordIsVisiable] = useState<boolean>(false);
 
   // have to make an effect for rerendering -> useState
   const [emailIsAbled, setEmailIsAbled] = useState<boolean>(true);
   const [phoneIsAbled, setPhoneIsAbled] = useState<boolean>(true);
+
+  const idValueRef = useRef<IdValueObject>({ inputEmail: '', inputPhone: '' });
+
+  const verifyEmail = () => {
+    const res = apis[0];
+    console.log(idValueRef.current['inputEmail']);
+    console.log(res);
+  };
 
   const validateInputValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
     const target = e.currentTarget;
@@ -44,6 +62,7 @@ const LoginForm = () => {
         }
       }
     }
+    idValueRef.current[target.id] = target.value;
   };
 
   const handleOnBlur = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -69,47 +88,77 @@ const LoginForm = () => {
       <div>
         LoginForm
         <div>
-          tab
-          <button>이메일 주소</button>
-          <button>전화번호</button>
+          {tabState}
+          <button
+            id='tabEmail'
+            onClick={() => {
+              setTabState('tabEmail');
+              console.log(idValueRef.current);
+            }}
+          >
+            이메일 주소
+          </button>
+          <button
+            id='tabPhone'
+            onClick={() => {
+              setTabState('tabPhone');
+              console.log(idValueRef.current);
+            }}
+          >
+            전화번호
+          </button>
         </div>
-        <div>
-          EmailLogin
-          <input
-            id='inputEmail'
-            type='text'
-            onFocus={() => setEmailIsAbled(true)}
-            onChange={(e) => validateInputValue(e)}
-            onBlur={(e) => handleOnBlur(e)}
-          />
-          {!emailIsAbled && <div>error!!!!</div>}
-        </div>
-        <div>
-          PhoneLogin
-          <input
-            id='inputPhone'
-            type='text'
-            onFocus={() => setPhoneIsAbled(true)}
-            onChange={(e) => validateInputValue(e)}
-            onBlur={(e) => handleOnBlur(e)}
-          />
-          {!phoneIsAbled && <div>error!!!!</div>}
-        </div>
-        <button
-          type='submit'
-          onClick={() => console.log('login')}
-          disabled={emailError}
-        >
-          다음(이메일)
-        </button>
-        <button
-          type='submit'
-          onClick={() => console.log('login')}
-          disabled={phoneError}
-        >
-          다음(폰)
-        </button>
+        {tabState === 'tabEmail' ? (
+          <div>
+            EmailLogin
+            <input
+              id='inputEmail'
+              type='text'
+              onFocus={() => setEmailIsAbled(true)}
+              onChange={(e) => validateInputValue(e)}
+              onBlur={(e) => handleOnBlur(e)}
+              defaultValue={idValueRef.current['inputEmail']}
+              key={'inputEmailKeyValue'}
+            />
+            {!emailIsAbled && <div>유효한 이메일 주소를 입력하세요.</div>}
+            <button
+              type='submit'
+              onClick={() => verifyEmail()}
+              disabled={emailError}
+            >
+              다음(이메일)
+            </button>
+          </div>
+        ) : (
+          <div>
+            PhoneLogin
+            <input
+              id='inputPhone'
+              type='text'
+              onFocus={() => setPhoneIsAbled(true)}
+              onChange={(e) => validateInputValue(e)}
+              onBlur={(e) => handleOnBlur(e)}
+              defaultValue={idValueRef.current['inputPhone']}
+              key={'inputPhoneKeyValue'}
+            />
+            {!phoneIsAbled && <div>유효한 전화번호를 입력하세요.</div>}
+            <button
+              type='submit'
+              onClick={() => console.log('login')}
+              disabled={phoneError}
+            >
+              다음(폰)
+            </button>
+          </div>
+        )}
         <button>Google</button>
+      </div>
+      <div>
+        <input id='password' type={passwordIsVisiable ? 'text' : 'password'} />
+        <button onClick={() => setPasswordIsVisiable(!passwordIsVisiable)}>
+          {passwordIsVisiable ? <div>On</div> : <div>Off</div>}
+        </button>
+        <button>다음(패스워드)</button>
       </div>
     </>
   );
