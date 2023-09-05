d2940fe79a89f84252f262b142c60bd81bc3d90e
Owon <chlehdnjs@gmail.com>
2023-09-06 00:23:56 +0900

    feat: feat msw broser setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import { worker } from './mocks/broswer.ts';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
